5ff8a0bd75e8387256df46094e413a36999fd58a
Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
2022-08-03 01:07:59 +0700

    pdx20x: pdx206: load all missing modules
    exfat, led and bu520x1nvx input driver should works
    
    Signed-off-by: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -1,5 +1,8 @@
 #!/sbin/sh
 
 setenforce 0
-insmod /vendor/lib/modules/1.1/sec_touchscreen.ko
+insmod /vendor/lib/modules/sec_touchscreen.ko
+insmod /vendor/lib/modules/texfat.ko
+insmod /vendor/lib/modules/tcs3490.ko
+insmod /vendor/lib/modules/bu520x1nvx.ko
 echo 1 > /sys/devices/virtual/sec/tsp/after_work
