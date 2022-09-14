00a55891854fdafae196f0a90d5e271724d18054
lolipuru <banstellazz@gmail.com>
2022-09-14 16:40:05 -0400

    pdx206: fix android 13 decryption, sdcard and otg
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -8,8 +8,8 @@ include $(CLEAR_VARS)
 
     LOCAL_POST_INSTALL_CMD += \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules; \
-        cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/modules $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/; \
+        cp -rf $(LOCAL_PATH)/pdx206/module $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/; \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware; \
-        cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/firmware $(TARGET_RECOVERY_ROOT_OUT)/vendor/;
+        cp -rf $(LOCAL_PATH)/pdx206/firmware $(TARGET_RECOVERY_ROOT_OUT)/vendor/;
 
 include $(BUILD_PHONY_PACKAGE)
