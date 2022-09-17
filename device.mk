58161545bd86c4c654858741fb12c75c1469c8c2
lolipuru <banstellazz@gmail.com>
2022-09-17 16:10:53 -0400

    pdx206: fix ota for virtual a/b
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Configure core_64_bit_only.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
+# Configure gsi_keys.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Configure twrp
 $(call inherit-product, vendor/twrp/config/common.mk)
 
