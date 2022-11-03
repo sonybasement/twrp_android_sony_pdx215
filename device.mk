086cbc1ea79586cc58b857184494d8ce88faf18c
Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
2022-11-04 05:56:17 +0700

    pdx206: cleanup
    - slotselect works now, but need to define /mapper path due to how sony implement broken super partition
    - haptic is not working, due to being require 1.2 to works
    - clean up unused blobs which can be link later if needed
    - got many complain about how flag use uppercase in xperia dev. group, so
    - thanks @ sekaiacg for https://github.com/sekaiacg/android_device_xiaomi_umi_TWRP/commit/e4792520af08d0f2c0233b1599e177a4afe0a9f0 and https://github.com/sekaiacg/android_device_xiaomi_umi_TWRP/commit/6aee51da2f65dec80c6be75fd15f16c4394bed72
    
    Signed-off-by: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -45,13 +45,20 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     update_engine \
     update_engine_sideload \
-    update_verifier
+    update_verifier \
+    checkpoint_gc
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
     bootctrl.sony_sm8250 \
